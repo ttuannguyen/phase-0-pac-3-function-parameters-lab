e7f2039ca29a015551e8facede97108ec43fc80b
Tuan Nguyen <trentontuannguyen@gmail.com>
2021-11-11 15:36:26 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+function introduction(name) {
+    return `Hi, my name is ${name}.`;
+}
+//introduction("Tuan");
+
+function introductionWithLanguage(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+console.log(introductionWithLanguageOptional("Tuan"));
